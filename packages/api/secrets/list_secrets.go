352255e693af3941bc966546e4556f532aacff70
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-06 01:40:42 +0200

    Better errors

--- packages/api/secrets/list_secrets.go
+++ packages/api/secrets/list_secrets.go
@@ -1,24 +1,38 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/go-resty/resty/v2"
 )
 
-func CallListSecretsV3(httpClient *resty.Client, request ListSecretsOptions) (ListSecretsResponse, error) {
+func CallListSecretsV3(httpClient *resty.Client, request ListSecretsRequest) (ListSecretsResponse, error) {
 
 	secretsResponse := ListSecretsResponse{}
 
-	response, err := httpClient.R().
+	req := httpClient.R().
 		SetResult(&secretsResponse).
-		SetBody(request).
-		Post("/v1/auth/universal-auth/login")
+		SetQueryParams(map[string]string{
+			"workspaceId":            request.ProjectId,
+			"workspaceSlug":          request.ProjectSlug,
+			"environment":            request.Environment,
+			"expandSecretReferences": fmt.Sprintf("%t", request.ExpandSecretReferences),
+			"include_imports":        fmt.Sprintf("%t", request.IncludeImports),
+			"recursive":              fmt.Sprintf("%t", request.Recursive),
+		})
 
-	if err != nil {
-		return ListSecretsResponse{}, err
+	if request.SecretPath != "" {
+		req.SetQueryParam("secretPath", request.SecretPath)
 	}
 
-	if response.IsError() {
-		return ListSecretsResponse{}, response.Error().(error)
+	res, err := req.Get("/v3/secrets/raw")
+
+	if err != nil {
+		return ListSecretsResponse{}, fmt.Errorf("CallListSecretsV3: Unable to complete api request [err=%s]", err)
+	}
+
+	if res.IsError() {
+		return ListSecretsResponse{}, fmt.Errorf("CallListSecretsV3: Unsuccessful response [%v %v] [status-code=%v]", res.Request.Method, res.Request.URL, res.StatusCode())
 	}
 
 	return secretsResponse, nil
