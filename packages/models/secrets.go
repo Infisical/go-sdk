54c0f6ed53d438006fbc7e56c7072d18ece9fbe2
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-06 01:40:12 +0200

    Secrets model

---
+++ packages/models/secrets.go
@@ -0,0 +1,20 @@
+package models
+
+type Secret struct {
+	ID            string `json:"id"`
+	Workspace     string `json:"workspace"`
+	Environment   string `json:"environment"`
+	Version       int    `json:"version"`
+	Type          string `json:"type"`
+	SecretKey     string `json:"secretKey"`
+	SecretValue   string `json:"secretValue"`
+	SecretComment string `json:"secretComment"`
+	SecretPath    string `json:"secretPath,omitempty"`
+}
+
+type SecretImport struct {
+	SecretPath  string   `json:"secretPath"`
+	Environment string   `json:"environment"`
+	FolderID    string   `json:"folderId"`
+	Secrets     []Secret `json:"secrets"`
+}
