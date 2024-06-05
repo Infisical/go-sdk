a172b998ea6f7c30f1a7a41765d2da9c477e2803
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-05 23:05:56 +0200

    Create constants.go

---
+++ packages/util/constants.go
@@ -0,0 +1,44 @@
+package util
+
+const (
+
+	// Universal auth:
+	INFISICAL_UNIVERSAL_AUTH_CLIENT_ID_ENV_NAME     = "INFISICAL_UNIVERSAL_AUTH_CLIENT_ID"
+	INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET_ENV_NAME = "INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET"
+
+	// GCP auth:
+	INFISICAL_GCP_AUTH_IDENTITY_ID_ENV_NAME                  = "INFISICAL_GCP_AUTH_IDENTITY_ID"
+	INFISICAL_GCP_IAM_SERVICE_ACCOUNT_KEY_FILE_PATH_ENV_NAME = "INFISICAL_GCP_IAM_SERVICE_ACCOUNT_KEY_FILE_PATH"
+
+	// AWS auth:
+	INFISICAL_AWS_IAM_AUTH_IDENTITY_ID_ENV_NAME = "INFISICAL_AWS_IAM_AUTH_IDENTITY_ID"
+
+	// Azure auth:
+	INFISICAL_AZURE_AUTH_IDENTITY_ID_ENV_NAME = "INFISICAL_AZURE_AUTH_IDENTITY_ID"
+
+	// Kubernetes auth:
+	INFISICAL_KUBERNETES_IDENTITY_ID_ENV_NAME                = "INFISICAL_KUBERNETES_IDENTITY_ID"
+	INFISICAL_KUBERNETES_SERVICE_ACCOUNT_TOKEN_PATH_ENV_NAME = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+
+	// Access token:
+	INFISICAL_ACCESS_TOKEN_ENV_NAME = "INFISICAL_ACCESS_TOKEN"
+
+	// AWS metadata service:
+	AWS_EC2_METADATA_TOKEN_URL             = "http://169.254.169.254/latest/api/token"
+	AWS_EC2_INSTANCE_IDENTITY_DOCUMENT_URL = "http://169.254.169.254/latest/dynamic/instance-identity/document"
+
+	// Azure metadata service:
+	AZURE_METADATA_SERVICE_URL = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F"
+)
+
+type AuthMethod string
+
+const (
+	ACCESS_TOKEN   AuthMethod = "ACCESS_TOKEN"
+	UNIVERSAL_AUTH AuthMethod = "UNIVERSAL_AUTH"
+	GCP_ID_TOKEN   AuthMethod = "GCP_ID_TOKEN"
+	GCP_IAM        AuthMethod = "GCP_IAM"
+	AWS_IAM        AuthMethod = "AWS_IAM"
+	KUBERNETES     AuthMethod = "KUBERNETES"
+	AZURE          AuthMethod = "AZURE"
+)
