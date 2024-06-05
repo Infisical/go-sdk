bda858ca080086e99e94180d6ecaaf84fa9fea5a
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-06 01:41:10 +0200

    Ensure unique secrets for recursive mode

--- packages/util/helper.go
+++ packages/util/helper.go
@@ -1,6 +1,10 @@
 package util
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/infisical/go-sdk/packages/models"
+)
 
 func AppendAPIEndpoint(siteUrl string) string {
 	// Ensure the address does not already end with "/api"
@@ -13,4 +17,31 @@ func AppendAPIEndpoint(siteUrl string) string {
 		return siteUrl + "api"
 	}
 	return siteUrl + "/api"
+}
+
+func EnsureUniqueSecretsByKey(secrets *[]models.Secret) {
+	secretMap := make(map[string]models.Secret)
+
+	// Move secrets to a map to ensure uniqueness
+	for _, secret := range *secrets {
+		secretMap[secret.SecretKey] = secret // Maps will overwrite existing entry with the same key
+	}
+
+	// Clear the slice
+	*secrets = (*secrets)[:0]
+
+	// Refill the slice from the map
+	for _, secret := range secretMap {
+		*secrets = append(*secrets, secret)
+	}
+}
+
+// containsSecret checks if the given key exists in the slice of secrets
+func ContainsSecret(secrets []models.Secret, key string) bool {
+	for _, secret := range secrets {
+		if secret.SecretKey == key {
+			return true
+		}
+	}
+	return false
 }
