ee38998871b5f6289e8114fac40429facb2a6883
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-06 02:04:33 +0200

    Finished List secrets

--- secrets.go
+++ secrets.go
@@ -1,21 +1,59 @@
 package infisical
 
-import api "github.com/infisical/go-sdk/packages/api/secrets"
+import (
+	"os"
 
-type ListSecretOptions = api.ListSecretsOptions
+	api "github.com/infisical/go-sdk/packages/api/secrets"
+	"github.com/infisical/go-sdk/packages/models"
+	"github.com/infisical/go-sdk/packages/util"
+)
+
+type ListSecretsOptions = api.ListSecretsRequest
 
 type SecretsInterface interface {
-	List(options ListSecretOptions) string
+	List(options ListSecretsOptions) ([]models.Secret, error)
 }
 
 type Secrets struct {
 	client *Client
 }
 
-func (s *Secrets) List(options ListSecretOptions) string {
-	api.CallListSecretsV3(s.client.httpClient, options)
+func (s *Secrets) List(options ListSecretsOptions) ([]models.Secret, error) {
+	res, err := api.CallListSecretsV3(s.client.httpClient, options)
 
-	return "ListSecrets"
+	if err != nil {
+		return nil, err
+	}
+
+	if options.Recursive {
+		util.EnsureUniqueSecretsByKey(&res.Secrets)
+	}
+
+	secrets := append([]models.Secret(nil), res.Secrets...) // Clone main secrets slice, we will modify this if imports are enabled
+	if options.IncludeImports {
+
+		// Append secrets from imports
+		for _, importBlock := range res.Imports {
+			for _, importSecret := range importBlock.Secrets {
+				// Only append the secret if it is not already in the list, imports take precedence
+				if !util.ContainsSecret(secrets, importSecret.SecretKey) {
+					secrets = append(secrets, importSecret)
+				}
+			}
+		}
+	}
+
+	if options.AttachToProcessEnv {
+		for _, secret := range secrets {
+			// Only set the environment variable if it is not already set
+			if os.Getenv(secret.SecretKey) == "" {
+				os.Setenv(secret.SecretKey, secret.SecretValue)
+			}
+		}
+
+	}
+
+	return secrets, nil
 }
 
 func NewSecrets(client *Client) SecretsInterface {
