11a5f61696404d84e884630b241db869aa5f0023
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-07 03:27:45 +0200

    Error improvements

--- packages/util/helper.go
+++ packages/util/helper.go
@@ -1,8 +1,10 @@
 package util
 
 import (
+	"encoding/json"
 	"strings"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/infisical/go-sdk/packages/models"
 )
 
@@ -44,4 +46,35 @@ func ContainsSecret(secrets []models.Secret, key string) bool {
 		}
 	}
 	return false
+}
+
+/*
+If the status code is 400, there will most likely always be a body.
+The body is a json object with a message key. we need to try to parse it, but if it fails, we can just return an empty string.
+But if the status code is 500, there may not be a body. if there is, it will be a json object with a message key. we need to try to parse it, but if it fails, we can just return an empty string
+*/
+func TryParseErrorBody(res *resty.Response) string {
+	if res == nil || !res.IsError() {
+		return "No Error Message"
+	}
+
+	body := res.String()
+	if body == "" {
+		return "No Error Message"
+	}
+
+	type ErrorResponse struct {
+		Message string `json:"message"`
+	}
+
+	// now we have a string, we need to try to parse it as json
+	var errorResponse ErrorResponse
+	err := json.Unmarshal([]byte(body), &errorResponse)
+
+	if err != nil {
+		return "No Error Message"
+	}
+
+	return errorResponse.Message
+
 }
