885cca283c51ce5acafb928645768065022adf8b
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-08 01:25:01 +0200

    Update aws_auth_test.go

--- test/aws_auth_test.go
+++ test/aws_auth_test.go
@@ -10,14 +10,14 @@ import (
 func TestAWSAuthLogin(t *testing.T) {
 
 	client, err := infisical.NewInfisicalClient(infisical.Config{
-		// SiteUrl: "http://localhost:8080",
+		SiteUrl: "https://c61b724baab4.ngrok.app",
 	})
 
 	if err != nil {
 		t.Fatalf("Failed to create Infisical client: %v", err)
 	}
 
-	accessToken, err := client.Auth().AwsIamAuthLogin("0e007fbe-7954-48f9-b888-6665eec088e7") // test ID
+	accessToken, err := client.Auth().AwsIamAuthLogin("e2cddb75-a0e0-4c89-bfc0-4d536599f725") // test ID
 
 	if err != nil {
 		panic(err)
