815123f68fab5e61e8cd7dd264defb7c8189fda3
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-07 03:26:49 +0200

    Feat: Kubernetes auth

---
+++ packages/api/auth/kubernetes_auth_login.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func CallKubernetesAuthLogin(httpClient *resty.Client, request KubernetesAuthLoginRequest) (accessToken string, e error) {
+	var responseData GenericAuthLoginResponse
+
+	response, err := httpClient.R().
+		SetResult(&responseData).
+		SetBody(request).
+		Post("/v1/auth/kubernetes-auth/login")
+
+	if err != nil {
+		return "", fmt.Errorf("CallUniversalAuthLogin: Unable to complete api request [err=%s]", err)
+	}
+
+	if response.IsError() {
+		return "", fmt.Errorf("CallUniversalAuthLogin: Unsuccessful response [%v %v] [status-code=%v]", response.Request.Method, response.Request.URL, response.StatusCode())
+	}
+
+	return responseData.AccessToken, nil
+}
