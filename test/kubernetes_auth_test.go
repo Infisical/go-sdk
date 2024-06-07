0f6de306fde3ccff86760ece8b21670bd09d1d7a
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-08 00:32:59 +0200

    Update kubernetes_auth_test.go

--- test/kubernetes_auth_test.go
+++ test/kubernetes_auth_test.go
@@ -1,42 +1,49 @@
 package test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	infisical "github.com/infisical/go-sdk"
-)
+// 	infisical "github.com/infisical/go-sdk"
+// )
 
-func TestKubernetesAuthLogin(t *testing.T) {
+// func TestKubernetesAuthLogin(t *testing.T) {
 
-	t.Skip("Skipping Kubernetes Auth test -- requires running in a Kubernetes cluster")
+// 	t.Skip("Skipping Kubernetes Auth test -- requires running in a Kubernetes cluster")
 
-	client, err := infisical.NewInfisicalClient(infisical.Config{
-		SiteUrl: "http://localhost:8080",
-		Auth: infisical.Authentication{
-			Kubernetes: infisical.KubernetesAuth{
-				IdentityID:              "K8_MACHINE_IDENTITY_ID",
-				ServiceAccountTokenPath: "/var/run/secrets/kubernetes.io/serviceaccount/token", // Optional
-			},
-		},
-	})
+// 	client, err := infisical.NewInfisicalClient(infisical.Config{
+// 		SiteUrl: "http://localhost:8080",
+// 		Auth: infisical.Authentication{
+// 			Kubernetes: infisical.KubernetesAuth{
+// 				IdentityID:              "K8_MACHINE_IDENTITY_ID",
+// 				ServiceAccountTokenPath: "/var/run/secrets/kubernetes.io/serviceaccount/token", // Optional
+// 			},
+// 		},
+// 	})
 
-	if err != nil {
-		t.Fatalf("Failed to create Infisical client: %v", err)
-	}
+// 	// token1, err := client1.auth.kubernetesLogin(...)
+// 	// token2, err := client2.auth.kubernetesLogin(...)
 
-	secrets, err := client.Secrets().List(infisical.ListSecretsOptions{
-		ProjectID:   "PROJECT_ID",
-		Environment: "ENV_SLUG",
-	})
+// 	// handle err
 
-	if err != nil {
-		t.Fatalf("Failed to list secrets: %v", err)
-	}
+// 	// client.auth.setToken(token)
 
-	if len(secrets) == 0 {
-		t.Fatalf("No secrets found")
-	}
+// 	if err != nil {
+// 		t.Fatalf("Failed to create Infisical client: %v", err)
+// 	}
 
-	t.Logf("Secrets: %v", secrets)
+// 	secrets, err := client.Secrets().List(infisical.ListSecretsOptions{
+// 		ProjectID:   "PROJECT_ID",
+// 		Environment: "ENV_SLUG",
+// 	})
 
-}
+// 	if err != nil {
+// 		t.Fatalf("Failed to list secrets: %v", err)
+// 	}
+
+// 	if len(secrets) == 0 {
+// 		t.Fatalf("No secrets found")
+// 	}
+
+// 	t.Logf("Secrets: %v", secrets)
+
+// }
