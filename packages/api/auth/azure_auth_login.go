d25344e16d8bf259b51696776bb30830a4647659
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-08 00:32:49 +0200

    Create azure_auth_login.go

---
+++ packages/api/auth/azure_auth_login.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func CallAzureAuthLogin(httpClient *resty.Client, request AzureAuthLoginRequest) (accessToken string, e error) {
+	var responseData GenericAuthLoginResponse
+
+	response, err := httpClient.R().
+		SetResult(&responseData).
+		SetBody(request).
+		Post("/v1/auth/azure-auth/login")
+
+	if err != nil {
+		return "", fmt.Errorf("CallUniversalAuthLogin: Unable to complete api request [err=%s]", err)
+	}
+
+	if response.IsError() {
+		return "", fmt.Errorf("CallUniversalAuthLogin: Unsuccessful response [%v %v] [status-code=%v]", response.Request.Method, response.Request.URL, response.StatusCode())
+	}
+
+	return responseData.AccessToken, nil
+}
