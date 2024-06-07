ba1f9128edfb68cdb907dc4a572990aab620d868
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-06-08 00:32:40 +0200

    Update client.go

--- client.go
+++ client.go
@@ -1,9 +1,6 @@
 package infisical
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/go-resty/resty/v2"
 	"github.com/infisical/go-sdk/packages/util"
 )
@@ -14,87 +11,23 @@ type InfisicalClient struct {
 	config     Config
 
 	secrets SecretsInterface
+	auth    AuthInterface
 }
 
 type InfisicalClientInterface interface {
 	Secrets() SecretsInterface
+	Auth() AuthInterface
 }
 
 type Config struct {
-	CacheTtl  int
 	SiteUrl   string
-	Auth      Authentication
 	UserAgent string // optional, we set this when instantiating the client in the k8s operator / cli.
 }
 
-func ValidateAuth(ao *Authentication) (util.AuthMethod, error) {
-	if ao.UniversalAuth.ClientID != "" && ao.UniversalAuth.ClientSecret != "" {
-		return util.UNIVERSAL_AUTH, nil
-	} else if ao.GCPIdToken.IdentityID != "" {
-		return util.GCP_ID_TOKEN, nil
-	} else if ao.GCPIam.IdentityID != "" && ao.GCPIam.ServiceAccountKeyFilePath != "" {
-		return util.GCP_IAM, nil
-	} else if ao.AWSIam.IdentityID != "" {
-		return util.AWS_IAM, nil
-	} else if ao.Azure.IdentityID != "" {
-		return util.AZURE, nil
-	} else if ao.Kubernetes.IdentityID != "" && ao.Kubernetes.ServiceAccountTokenPath != "" {
-		return util.KUBERNETES, nil
-	} else if ao.AccessToken != "" {
-		return util.ACCESS_TOKEN, nil
-	}
-
-	// Read all potential environment variables
-
-	// Universal auth:
-	universalAuthClientIdEnv := os.Getenv(util.INFISICAL_UNIVERSAL_AUTH_CLIENT_ID_ENV_NAME)
-	universalAuthClientSecretEnv := os.Getenv(util.INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET_ENV_NAME)
-
-	// GCP ID token auth:
-	gcpIdTokenIdentityIdEnv := os.Getenv(util.INFISICAL_GCP_AUTH_IDENTITY_ID_ENV_NAME)
-
-	// GCP IAM auth:
-	gcpIamIdentityIdEnv := os.Getenv(util.INFISICAL_GCP_IAM_SERVICE_ACCOUNT_KEY_FILE_PATH_ENV_NAME)
-
-	// AWS IAM auth:
-	awsIamIdentityIdEnv := os.Getenv(util.INFISICAL_AWS_IAM_AUTH_IDENTITY_ID_ENV_NAME)
-
-	// Azure auth:
-	azureIdentityIdEnv := os.Getenv(util.INFISICAL_AZURE_AUTH_IDENTITY_ID_ENV_NAME)
-
-	// Kubernetes auth:
-	kubernetesIdentityIdEnv := os.Getenv(util.INFISICAL_KUBERNETES_IDENTITY_ID_ENV_NAME)
-	kubernetesServiceAccountTokenPathEnv := os.Getenv(util.INFISICAL_KUBERNETES_SERVICE_ACCOUNT_TOKEN_PATH_ENV_NAME)
-
-	// Access token:
-	accessTokenEnv := os.Getenv(util.INFISICAL_ACCESS_TOKEN_ENV_NAME)
-
-	if universalAuthClientIdEnv != "" && universalAuthClientSecretEnv != "" {
-		ao.UniversalAuth.ClientID = universalAuthClientIdEnv
-		ao.UniversalAuth.ClientSecret = universalAuthClientSecretEnv
-		return util.UNIVERSAL_AUTH, nil
-	} else if gcpIdTokenIdentityIdEnv != "" {
-		ao.GCPIdToken.IdentityID = gcpIdTokenIdentityIdEnv
-		return util.GCP_ID_TOKEN, nil
-	} else if gcpIamIdentityIdEnv != "" {
-		ao.GCPIam.IdentityID = gcpIamIdentityIdEnv
-		return util.GCP_IAM, nil
-	} else if awsIamIdentityIdEnv != "" {
-		ao.AWSIam.IdentityID = awsIamIdentityIdEnv
-		return util.AWS_IAM, nil
-	} else if azureIdentityIdEnv != "" {
-		ao.Azure.IdentityID = azureIdentityIdEnv
-		return util.AZURE, nil
-	} else if kubernetesIdentityIdEnv != "" && kubernetesServiceAccountTokenPathEnv != "" {
-		ao.Kubernetes.IdentityID = kubernetesIdentityIdEnv
-		ao.Kubernetes.ServiceAccountTokenPath = kubernetesServiceAccountTokenPathEnv
-		return util.KUBERNETES, nil
-	} else if accessTokenEnv != "" {
-		ao.AccessToken = accessTokenEnv
-		return util.ACCESS_TOKEN, nil
-	}
-
-	return "", fmt.Errorf("no authentication method is set")
+func (c *InfisicalClient) setAccessToken(accessToken string, authMethod util.AuthMethod) {
+	c.authMethod = authMethod
+	c.httpClient.SetAuthScheme("Bearer")
+	c.httpClient.SetAuthToken(accessToken)
 }
 
 func NewInfisicalClient(config Config) (InfisicalClientInterface, error) {
@@ -102,29 +35,15 @@ func NewInfisicalClient(config Config) (InfisicalClientInterface, error) {
 	if config.UserAgent == "" {
 		config.UserAgent = "infisical-go-sdk"
 	}
-
 	if config.SiteUrl == "" {
 		config.SiteUrl = util.DEFAULT_INFISICAL_API_URL
 	}
 
 	config.SiteUrl = util.AppendAPIEndpoint(config.SiteUrl)
 
-	// Auth method validation
-	authMethod, err := ValidateAuth(&config.Auth)
-
-	if err != nil {
-		return nil, fmt.Errorf("error while instantiating Infisical client: %v", err)
-	}
-
 	client := &InfisicalClient{
-		authMethod: authMethod,
 		config:     config,
 		httpClient: resty.New().SetHeader("User-Agent", config.UserAgent).SetBaseURL(config.SiteUrl),
-	}
-
-	authErr := client.authenticateHttpClient()
-	if authErr != nil {
-		return nil, fmt.Errorf("error while instantiating Infisical client: %v", authErr)
 	}
 
 	// add interfaces here
@@ -136,4 +55,8 @@ func NewInfisicalClient(config Config) (InfisicalClientInterface, error) {
 
 func (c *InfisicalClient) Secrets() SecretsInterface {
 	return c.secrets
+}
+
+func (c *InfisicalClient) Auth() AuthInterface {
+	return c.auth
 }
