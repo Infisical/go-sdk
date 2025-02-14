babe9bf6856eed83785394157e76b5215d80ee8d
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-02-15 02:26:38 +0800

    misc: added metadata to response

--- packages/models/secrets.go
+++ packages/models/secrets.go
@@ -1,15 +1,21 @@
 package models
 
+type SecretMetadata struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 type Secret struct {
-	ID            string `json:"id"`
-	Workspace     string `json:"workspace"`
-	Environment   string `json:"environment"`
-	Version       int    `json:"version"`
-	Type          string `json:"type"`
-	SecretKey     string `json:"secretKey"`
-	SecretValue   string `json:"secretValue"`
-	SecretComment string `json:"secretComment"`
-	SecretPath    string `json:"secretPath,omitempty"`
+	ID             string           `json:"id"`
+	Workspace      string           `json:"workspace"`
+	Environment    string           `json:"environment"`
+	Version        int              `json:"version"`
+	Type           string           `json:"type"`
+	SecretKey      string           `json:"secretKey"`
+	SecretValue    string           `json:"secretValue"`
+	SecretComment  string           `json:"secretComment"`
+	SecretPath     string           `json:"secretPath,omitempty"`
+	SecretMetadata []SecretMetadata `json:"secretMetadata"`
 }
 
 type SecretImport struct {
